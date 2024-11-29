53a22a20c26e326bb29ca975c391a9cb5c167175
Mustafa Kaan Güngör <kaan062004+github@gmail.com>
2024-11-29 11:01:50 +0300

    tried adding ricochet but failed miserably

--- Assets/_Scripts/PlayerTurret.cs
+++ Assets/_Scripts/PlayerTurret.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 
 public class PlayerTurret : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class PlayerTurret : MonoBehaviour
     private bool isReloaded;
     [SerializeField] float reloadTime = 3f;
     [SerializeField] GameObject hitEffect;
+    private CinemachineImpulseSource impulseSource;
 
     private void Awake() {
         playerControls = new PlayerControls();
@@ -19,6 +21,7 @@ public class PlayerTurret : MonoBehaviour
         playerControls.GameplayMap.Mouse1.performed += Shoot;
 
         turretAnimator = GetComponent<Animator>();
+        impulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
     private void OnDisable() {
@@ -55,9 +58,18 @@ public class PlayerTurret : MonoBehaviour
             var hit = Physics2D.Raycast(aimPoint.position, transform.up, maxRange);
             var trail = Instantiate(bulletTrail, aimPoint.position, transform.rotation);
             var trailScript = trail.GetComponent<BulletTrail>();
+            impulseSource.GenerateImpulseWithForce(0.5f);
+
             
             if(hit.collider != null) {
                 trailScript.SetTargetPos(hit.point);
+
+                Ray2D r = new Ray2D(aimPoint.position, hit.point);
+
+                float angle = Vector2.Angle(hit.normal, r.direction);
+                Debug.Log(angle);
+                Debug.DrawLine(hit.point, hit.point + hit.normal, Color.yellow, 5, false);
+                Debug.DrawRay(aimPoint.position, hit.point, Color.blue, 5, false);
 
                 Instantiate(hitEffect,hit.point, transform.rotation);
                 if(hit.collider.gameObject.TryGetComponent<Health>(out var health)) {
